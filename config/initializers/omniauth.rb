22173c7287a400b78a95f23cbe7e9c1a31dc3e01
Lê Xuân Mạnh <lxmanh@outlook.com>
2025-09-18 15:39:55 +0700

    Update latest upstream commits

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,39 +3,19 @@
 OmniAuth.config.full_host = ENV.fetch('FRONTEND_URL', 'http://localhost:3000')
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # OmniAuth.config.allowed_request_methods = [:get]
-  # # Google OAuth2
-  # provider :google_oauth2,
-  #          ENV.fetch('GOOGLE_OAUTH_CLIENT_ID', nil),
-  #          ENV.fetch('GOOGLE_OAUTH_CLIENT_SECRET', nil),
-  #          {
-  #            provider_ignores_state: true
-  #          }
+  # Google OAuth2
+  provider :google_oauth2,
+           ENV.fetch('GOOGLE_OAUTH_CLIENT_ID', nil),
+           ENV.fetch('GOOGLE_OAUTH_CLIENT_SECRET', nil),
+           {
+             provider_ignores_state: true
+           }
 
-  # UEF SSO (based on Keycloak)
-  # provider :keycloak_openid,
-  #          ENV.fetch('UEF_SSO_CLIENT_ID', nil),
-  #          ENV.fetch('UEF_SSO_CLIENT_SECRET', nil),
-  #          client_options: {
-  #            site: ENV.fetch('UEF_SSO_URL', nil),
-  #            realm: ENV.fetch('UEF_SSO_REALM', nil),
-  #            base_url: ''
-  #          },
-  #          name: 'uef'
-
-  # Using custom omniauth strategy for UEF ID
+  # UEF ID OAuth2
   provider :uefid,
            ENV.fetch('UEF_SSO_CLIENT_ID', nil),
            ENV.fetch('UEF_SSO_CLIENT_SECRET', nil),
            client_options: {
-             site: ENV.fetch('UEF_SSO_URL', nil),
-             realm: ENV.fetch('UEF_SSO_REALM', nil),
-             base_url: '',
-            #  redirect_uri: ENV.fetch('UEF_SSO_CALLBACK_URL', nil)
-           },
-           name: 'uef',
-           scope: 'openid email profile',
-           strategy_class: OmniAuth::Strategies::UefId,
-           skip_jwt: ENV.fetch('UEF_SSO_SKIP_JWT', 'false') == 'true'
-
+             # any other options such as site, authorize_url, token_url can be configured here
+           }
 end
